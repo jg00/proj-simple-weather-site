0f431755d32a8fa9fff0687c114a5ba65289c1b6
Jack Gamboa <jgamboa929@gmail.com>
2019-05-16 13:18:13 -0500

    Complete simple weather app.

---
+++ app.js
@@ -0,0 +1,42 @@
+searchButton.addEventListener("click", searchWeather);
+
+function searchWeather(e) {
+  loadingText.style.display = "block";
+  weatherBox.style.display = "none";
+
+  let cityName = searchCity.value;
+
+  if (cityName.trim().length === 0) {
+    return alert("Please enter City!");
+  }
+
+  let http = new XMLHttpRequest();
+  let apiKey = config.apiKey;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=metric&appid=${apiKey}`;
+  let method = "GET";
+
+  http.open(method, url);
+  http.onreadystatechange = function() {
+    if (http.readyState === XMLHttpRequest.DONE && http.status === 200) {
+      let data = JSON.parse(http.responseText);
+      let weatherData = new Weather(
+        cityName,
+        data.weather[0].description.toUpperCase()
+      );
+      weatherData.temperature = data.main.temp;
+      updateWeather(weatherData);
+    } else if (http.readyState === XMLHttpRequest.DONE) {
+      alert("Error occurred!");
+    }
+  };
+  http.send();
+}
+
+function updateWeather(weatherData) {
+  weatherCity.textContent = weatherData.cityName;
+  weatherDescription.textContent = weatherData.description;
+  weatherTemperature.textContent = weatherData.temperature;
+
+  loadingText.style.display = "none";
+  weatherBox.style.display = "block";
+}
---
+++ init.js
@@ -0,0 +1,9 @@
+let searchButton = document.querySelector("button");
+let searchCity = document.querySelector("#city");
+
+let loadingText = document.querySelector("#load");
+
+let weatherBox = document.querySelector("#weather");
+let weatherCity = weatherBox.firstElementChild;
+let weatherDescription = document.querySelector("#weatherDescription");
+let weatherTemperature = weatherBox.lastElementChild;
